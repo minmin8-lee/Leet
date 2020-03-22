9c20fc31bb8454fae87952b4378e5c5fc8db5417
minmin8-lee <59409959+minmin8-lee@users.noreply.github.com>
2020-03-22 18:55:25 +0900

    Update Solution.py

--- 20_Valid_Parentheses/Solution.py
+++ 20_Valid_Parentheses/Solution.py
@@ -1,30 +1,27 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        score = 0
         CODE = {"(": 1, "{": 1, "[": 1,
                 ")": -1, "}": -1, "]": -1}
         SET = {")": "(", "}": "{", "]": "["}
 
         for e in s:
-            if CODE[e] == 1:
-                score += CODE[e]
+            if CODE[e] > 0:
                 stack.append(e)
-            elif CODE[e] < 0:
-                score += CODE[e]
-                if score < 0:
-                    return False
-                elif stack[-1] == SET[e]:
-                    stack.pop()
-                else:
-                    return False
             else:
-                return False
-        if score == 0:
+                try:
+                    if stack.pop() == SET[e]:
+                        continue
+                    else:
+                        return False
+                except:
+                    return False
+        if not stack:
             return True
         else:
             return False
 
+
 if __name__ == '__main__':
-    s = Solution().isValid("[]")
+    s = Solution().isValid("[[[[[{}]]]]]({})[]")
     print(s)
